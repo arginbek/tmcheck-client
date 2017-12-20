6a2e4ae9d1c4c96006c3bb8ab497e892d71996ef
arginbek <arginbek@gmail.com>
2017-12-20 11:32:34 -0600

    Added Login
    Added Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,36 @@
 import { FormsModule } from '@angular/forms';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule} from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { UserService } from './services/user.service';
 import { UserComponent } from './user/user.component';
+import { LoginComponent } from './login/login.component';
+import { AlertComponent } from './directives/alert/alert.component';
+import { JwtInterceptor } from './interceptors/jwt.interceptor';
+import { AuthGuard } from './guards/auth.guard';
+import { AuthenticationService } from './services/authentication.service';
+import { AlertService } from './services/alert.service';
+import { HomeComponent } from './home/home.component';
 
 const appRoutes: Routes = [
+  {
+    path: '',
+    component: HomeComponent, canActivate: [AuthGuard]
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
   {
     path: 'users',
     component: UserComponent,
     data: { title: 'Users List' }
   },
-  { path: '',
+  {
+    path: '',
     redirectTo: '/users',
     pathMatch: 'full'
   }
@@ -23,7 +39,10 @@ const appRoutes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    LoginComponent,
+    AlertComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -32,7 +51,8 @@ const appRoutes: Routes = [
     RouterModule.forRoot(appRoutes)
   ],
   providers: [
-    UserService
+    UserService, { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    AuthGuard, AuthenticationService, AlertService
   ],
   bootstrap: [AppComponent]
 })
